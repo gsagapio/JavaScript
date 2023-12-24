956866523ea126513626991e62d1fea9a69b1ef3
Gustavo S. Agapio <gsilvaagapio123@gmail.com>
2023-12-24 10:21:43 -0300

    aulas09

--- cursoA/aula12/desafio2/script.js
+++ cursoA/aula12/desafio2/script.js
@@ -39,7 +39,7 @@ function verificar() {
             }
         }
 
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`;
+        = res.innerHTML `Detectamos ${genero} com ${idade} anos.`;
         res.appendChild(img);
     }
 }
